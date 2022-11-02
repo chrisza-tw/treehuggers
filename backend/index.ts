66b1189537ef8749bb3a8d3f84f3ca48ce219ea1
Serene <serene.quek@thoughtworks.com>
2022-11-02 10:07:38 +0800

    [huy&serene] added send notification

--- backend/index.ts
+++ backend/index.ts
@@ -10,6 +10,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8000;
 const serviceAccountKey = process.env.FIREBASE_SECRETS || ""
+const webpush = require('web-push');
 
 app.use(cors())
 app.use(router);
@@ -23,8 +24,37 @@ admin.initializeApp({
 
 const db = getFirestore();
 
+// VAPID keys should only be generated only once.
+const vapidKeys = {
+  publicKey: process.env.VAPID_PUBLIC_KEY,
+  privateKey: process.env.VAPID_PRIVATE_KEY
+};
+
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+const pushSubscription = 
+  {
+    "endpoint": "...",
+    "expirationTime":null,
+    "keys":
+    {
+      "p256dh":"...",
+      "auth":"..."
+    }
+  };
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/notification', async (req: Request, res: Response) => {
+  await webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+  console.log("message sent");
+  res.json({message: "message sent! "})
 });
 
 app.listen(port, () => {console.log(`⚡️[server]: Server is running at https://localhost:${port}`)});
--- frontend/sw.js
+++ frontend/sw.js
@@ -1,3 +1,5 @@
-self.addEventListener('push', () => {
-    self.registration.showNotification('Hello world!');
+self.addEventListener('push', (event) => {
+    event.waitUntil(
+        self.registration.showNotification('Hello world!')
+    )
 });

21a098f65bd0220f4a2991d0fbbefcd215390a9f
Serene <serene.quek@thoughtworks.com>
2022-11-03 17:12:41 +0800

    [serene&huy] modified sw

--- frontend/sw.js
+++ frontend/sw.js
@@ -4,14 +4,12 @@ cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
 
 self.addEventListener('push', (event) => {
-    const data = event.data;
-    let responseMsg = "Grid is dirty!!"
-    
-    if (data.body === "false"){
-        responseMsg = "Grid is clean!!"
-    }
-
+    const data = event.data.json();
     console.log(data);
+    const { gridStatus } = data;
+
+    const responseMsg = gridStatus ? "Grid is dirty!!" : "Grid is clean!!";
+
     event.waitUntil(
         self.registration.showNotification('TreeHugggers', {
             body: responseMsg
